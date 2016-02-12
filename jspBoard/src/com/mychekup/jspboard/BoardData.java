334503d16d78573f0edb3f18f3149fd408fe9869
atozjame <atozjames@gmail.com>
2016-02-13 00:15:44 +0900

    first update

---
+++ jspBoard/src/com/mychekup/jspboard/BoardData.java
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+package com.mychekup.jspboard;
+
+import java.io.*;
+import java.sql.*;
+
+/**
+ * @author wBom
+ *
+ */
+public class BoardData implements Serializable{
+
+	private int num;
+	private String name;
+	private String subject;
+	private String content;
+	private Date date;
+	private String password;
+	private int count;
+	private int ref;
+	private int step;
+	private int depth;
+	private int childCount;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getRef() {
+		return ref;
+	}
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+	public int getStep() {
+		return step;
+	}
+	public void setStep(int step) {
+		this.step = step;
+	}
+	public int getDepth() {
+		return depth;
+	}
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	public int getChildCount() {
+		return childCount;
+	}
+	public void setChildCount(int childCount) {
+		this.childCount = childCount;
+	}
+	
+	
+	
+}
