3c1f50374bd26f6596457dd45c750e93e9839374
Atozjames <atozjames@gmail.com>
2016-02-29 18:32:20 +0900

    propertise test

---
+++ jspSample/src/com/mycheckup/ProReader.java
@@ -0,0 +1,50 @@
+package com.mycheckup;
+
+import java.util.*;
+import java.io.*;
+
+public class ProReader {
+
+	Enumeration propNames=null;
+	Vector v = null;
+	
+	Properties prop=null;
+	
+	public void ProReader() {
+
+	}
+
+	public void read(){
+		
+		try{
+			
+			prop = new Properties();
+			
+			InputStream is =this.getClass().getClassLoader().getResourceAsStream("oracleDB.properties");
+			prop.load(is);
+			
+			
+			propNames= prop.propertyNames();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	
+		while(propNames.hasMoreElements()){
+			
+			String key = (String) propNames.nextElement();
+			
+			System.out.println(key);
+			System.out.println(prop.getProperty(key));
+			
+		}
+		
+	}
+	
+	public static void main(String[] agrs){
+		
+		ProReader read= new ProReader();
+		read.read();
+		
+	}
+}
